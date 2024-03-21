90017d7b3c285227f1d9de6fc2c012a2ccad3a6d
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2024-03-21 22:51:41 +0530

    added onboarding support

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useRef } from "react";
 import markdownStyles from "./markdown-styles.module.css";
 
 type Props = {
@@ -5,6 +7,35 @@ type Props = {
 };
 
 export function PostBody({ content }: Props) {
+  const useHighlightAndScroll = (text: string) => {
+    useEffect(() => {
+      if (!text) return;
+
+      const range = document.createRange();
+      const selection = window.getSelection();
+      const walker = document.createTreeWalker(
+        document.body,
+        NodeFilter.SHOW_TEXT,
+        null
+      );
+      const urlParams = new URLSearchParams(document.location.search);
+      const toOnboard = urlParams.get("onboard");
+      let node;
+      while ((node = walker.nextNode())) {
+        if (node.nodeValue?.includes(text) && toOnboard) {
+          range.selectNodeContents(node);
+          selection?.removeAllRanges();
+          selection?.addRange(range);
+          //@ts-ignore
+          node.parentNode?.scrollIntoView();
+          break;
+        }
+      }
+    }, [text]);
+  };
+
+  useHighlightAndScroll("Instructions For Use");
+
   return (
     <div className="max-w-2xl mx-auto">
       <div
